faba57e933e7629415ab9359517b7daae51cd459
Mark Lopez <m@silvenga.com>
2018-08-28 21:16:36 -0500

    Added coverage around the CleanupAgent.

--- tests/Emissary.Tests/Agents/CleanupAgent.cs
+++ tests/Emissary.Tests/Agents/CleanupAgent.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
+
+using AutoFixture;
 
 using Emissary.Agents;
 using Emissary.Core;
@@ -11,6 +14,8 @@ namespace Emissary.Tests.Agents
 {
     public class CleanupAgentFacts
     {
+        private static readonly Fixture Autofixture = new Fixture();
+
         private readonly IAgent _agent = new CleanupAgent();
         private readonly IContainerRegistrar _containerRegistrar;
 
@@ -19,10 +24,15 @@ namespace Emissary.Tests.Agents
             _containerRegistrar = Substitute.For<IContainerRegistrar>();
         }
 
-
         [Fact]
         public void When_the_a_cancelation_occurs_then_cleanup_existing_containers()
         {
+            var transaction = Substitute.For<IContainerRegistrarTransaction>();
+            _containerRegistrar.BeginTransaction().Returns(transaction);
+
+            var containers = Autofixture.Create<List<string>>();
+            transaction.GetContainers().Returns(containers);
+
             var source = new CancellationTokenSource();
             _agent.Monitor(_containerRegistrar, source.Token);
 
@@ -30,6 +40,10 @@ namespace Emissary.Tests.Agents
             source.Cancel();
 
             // Assert
+            foreach (var container in containers)
+            {
+                transaction.Received().DeleteContainer(container);
+            }
         }
     }
 }
